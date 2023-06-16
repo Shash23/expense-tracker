2e58d7e56a1827d3fa66643b019ccac84e45db5a
Shash <shashgk23@gmail.com>
2023-06-16 14:02:30 -0400

    added income schema

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -4,13 +4,7 @@ const IncomeSchema= require("../models/IncomeModel")
 exports.addIncome = async (req, res) => {
     const {title, amount, category, description, date}  = req.body
 
-    const income = IncomeSchema({
-        title,
-        amount,
-        category,
-        description,
-        date
-    })
+    const IncomeSchema
 
     try {
         //validations
